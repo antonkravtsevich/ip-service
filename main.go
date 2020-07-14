efb66aa69616a57eb4c3a92af2383e4dd06d52b9
Anton Kravtsevich <antonkravtsevich@gmail.com>
2020-07-14 19:57:35 +0300

    add json parsing and IP extraction in /logs endpoint

--- main.go
+++ main.go
@@ -2,11 +2,26 @@ package main
 
 import "fmt"
 import "net/http"
+import "encoding/json"
 
-func acceptJson(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "ok")
+type Log struct{
+	Ip	string
 }
 
+// Accept log record and extract IP address from it
+func acceptJson(w http.ResponseWriter, r *http.Request) {
+	var log Log
+
+	err := json.NewDecoder(r.Body).Decode(&log)
+	if err != nil{
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintf(w, log.Ip)
+}
+
+// Provide Prometheus metrics 
 func returnMetric(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "ok")
 }
