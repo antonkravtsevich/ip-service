e7fd2e6b1878ac564c8b232695316081172c1032
Anton Kravtsevich <antonkravtsevich@gmail.com>
2020-07-14 19:48:33 +0300

    add endpoints template

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
 import "fmt"
+import "net/http"
+
+func acceptJson(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "ok")
+}
+
+func returnMetric(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "ok")
+}
 
 func main() {
-	fmt.Println("Hello, world.")
+	acceptJsonServerMux := http.NewServeMux()
+    acceptJsonServerMux.HandleFunc("/logs", acceptJson)
+
+    returnMetricServer := http.NewServeMux()
+    returnMetricServer.HandleFunc("/metrics", returnMetric)
+
+    go func() {
+        http.ListenAndServe("localhost:5000", acceptJsonServerMux)
+    }()
+
+    http.ListenAndServe("localhost:9092", returnMetricServer)
 }
