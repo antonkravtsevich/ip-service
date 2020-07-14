bb64207689ed0750db86dc536f2545433b388417
Anton Kravtsevich <antonkravtsevich@gmail.com>
2020-07-14 21:59:02 +0300

    add prometheus integration, remove unnecessary functionality

--- main.go
+++ main.go
@@ -3,31 +3,16 @@ package main
 import "fmt"
 import "net/http"
 import "encoding/json"
-import "strconv"
-
-// types of commands that's can be used through main channel
-const (
-	AddIp = iota
-	GetCount
-)
-
-type CommandType int
-
-// Structure of commands, that will be used for data exchange between endpoints
-type Command struct{
-	commandType CommandType
-	payload string				// new IPs will be sent in this field
-	responseChannel	chan int  	// channel, that will be used to retrieve new data and for 
-								// /logs endpoint blocking in case if new IP should be added
-}
+import "github.com/prometheus/client_golang/prometheus"
+import "github.com/prometheus/client_golang/prometheus/promhttp"
 
 type Log struct{
 	Ip	string
 }
 
-// provide access to command channel for both endpoints
+// provide access to channel that will be used for IPs sharing
 type Server struct{
-	commandsChannel chan<- Command
+	ipsChan chan<-string
 }
 
 // check if element is in list
@@ -42,44 +27,31 @@ func isArrayContainValue(array []string, value string) bool{
 
 // check if IP is unique
 // return channel, that should be used for IPs values sending
-// calculate count of unique IPs 
-func startIPsCounter() chan <- Command{
+func startIPsCounter(ipsCounter prometheus.Counter) chan <- string{
 
 	// tree will be used as storage for all unique IPs
 	tree := Node{' ', []*Node{}}
 	// all unique IPs will be splitted in symbols, each symbol will be stored
 	// as node in tree
-	uniqueIpsCount := 0
 
-	commandsChannel := make(chan Command)
+	ipsChan := make(chan string)
 
 	// run commands handling
 	go func(){
-		for command := range commandsChannel{
-			switch command.commandType{
-			// handle command for new IP adding
-			case AddIp:
-				newIp := command.payload
+		for newIp := range ipsChan{
+			// split IP in symbols for further processing
+			newIpSplitted := []rune(newIp)
+			unique := !tree.isBranchExists(newIpSplitted)
 
-				// split IP in symbols for further processing
-				newIpSplitted := []rune(newIp)
-				
-				unique := !tree.isBranchExists(newIpSplitted)
-
-				// if IP unique - add it to tree, increment counter
-				if unique {
-					tree.insertBranch(newIpSplitted)
-					uniqueIpsCount += 1
-				}
-				command.responseChannel <- 0
-			// handle command for unique IPs count recieving
-			case GetCount:
-				command.responseChannel <- uniqueIpsCount
+			// if IP unique - add it to tree, increment counter
+			if unique {
+				tree.insertBranch(newIpSplitted)
+				ipsCounter.Inc()
 			}
 		}
 	}()
 
-	return commandsChannel
+	return ipsChan
 }
 
 // Accept log record and extract IP address from it
@@ -92,43 +64,30 @@ func (server *Server) acceptJson(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	responseChannel := make(chan int)
-
 	// send command with new IP, wait for response channel
-	command := Command{AddIp, log.Ip, responseChannel}
-	server.commandsChannel <- command
-	<- responseChannel
+	server.ipsChan <- log.Ip
 
 	fmt.Fprintf(w, "ok")
 }
 
-// Provide Prometheus metrics 
-func (server *Server) returnMetric(w http.ResponseWriter, r *http.Request) {
-	responseChannel := make(chan int)
-
-	// send command for unique IPs count retrieving, recieve value 
-	// in response channel
-	command := Command{GetCount, "", responseChannel}
-	server.commandsChannel <- command
-	count := <- responseChannel
-	fmt.Fprintf(w, strconv.Itoa(count))
-}
-
 func main() {
-	commandsChannel := startIPsCounter()
+	ipsCounter := prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "unique_ip_addresses",
+		})
+	prometheus.MustRegister(ipsCounter)
+
+	ipsChan := startIPsCounter(ipsCounter)
 	server := Server{
-		commandsChannel: commandsChannel,
+		ipsChan: ipsChan,
 	}
 
 	acceptJsonServerMux := http.NewServeMux()
     acceptJsonServerMux.HandleFunc("/logs", server.acceptJson)
 
-    returnMetricServer := http.NewServeMux()
-    returnMetricServer.HandleFunc("/metrics", server.returnMetric)
-
     go func() {
         http.ListenAndServe("localhost:5000", acceptJsonServerMux)
     }()
 
-    http.ListenAndServe("localhost:9102", returnMetricServer)
+    http.ListenAndServe("localhost:9102", promhttp.Handler())
 }
