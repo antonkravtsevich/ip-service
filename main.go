896af136c56688acd2724676bb173e3a6fc43162
Anton Kravtsevich <antonkravtsevich@gmail.com>
2020-07-14 20:43:33 +0300

    add unique IPs calculation, add connection between endpoints

--- main.go
+++ main.go
@@ -3,16 +3,34 @@ package main
 import "fmt"
 import "net/http"
 import "encoding/json"
+import "strconv"
+
+// types of commands that's can be used through main channel
+const (
+	AddIp = iota
+	GetCount
+)
+
+type CommandType int
+
+// Structure of commands, that will be used for data exchange between endpoints
+type Command struct{
+	commandType CommandType
+	payload string				// new IPs will be sent in this field
+	responseChannel	chan int  	// channel, that will be used to retrieve new data and for 
+								// /logs endpoint blocking in case if new IP should be added
+}
 
 type Log struct{
 	Ip	string
 }
 
-
+// provide access to command channel for both endpoints
 type Server struct{
-	ipsChan chan<- string
+	commandsChannel chan<- Command
 }
 
+// check if element is in list
 func isArrayContainValue(array []string, value string) bool{
 	for _, element := range array{
 		if element == value {
@@ -22,25 +40,41 @@ func isArrayContainValue(array []string, value string) bool{
 	return false
 }
 
-func isIPUnique() chan <- string{
+// check if IP is unique
+// return channel, that should be used for IPs values sending
+// calculate count of unique IPs 
+func startIPsCounter() chan <- Command{
+
+	// as a temporary solution, all unique IPs will be stored in list
+	// that will be used to check if received IP is unique or not
 	var uniqueIpsList []string
 	uniqueIpsCount := 0
 
-	ipsChan := make(chan string)
+	commandsChannel := make(chan Command)
 
+	// run commands handling
 	go func(){
+		for command := range commandsChannel{
+			switch command.commandType{
+			// handle command for new IP adding
+			case AddIp:
+				newIp := command.payload
+				unique := !isArrayContainValue(uniqueIpsList, newIp)
 
-		for newIp := range ipsChan{
-			unique := !isArrayContainValue(uniqueIpsList, newIp)
-			if unique {
-				uniqueIpsList = append(uniqueIpsList, newIp)
-				uniqueIpsCount += 1
-				fmt.Printf("new unique IP! %v\n", newIp)
+				// if IP unique - add it to list, increment counter
+				if unique {
+					uniqueIpsList = append(uniqueIpsList, newIp)
+					uniqueIpsCount += 1
+				}
+				command.responseChannel <- 0
+			// handle command for unique IPs count recieving
+			case GetCount:
+				command.responseChannel <- uniqueIpsCount
 			}
 		}
 	}()
 
-	return ipsChan
+	return commandsChannel
 }
 
 // Accept log record and extract IP address from it
@@ -53,20 +87,32 @@ func (server *Server) acceptJson(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	server.ipsChan <- log.Ip
+	responseChannel := make(chan int)
+
+	// send command with new IP, wait for response channel
+	command := Command{AddIp, log.Ip, responseChannel}
+	server.commandsChannel <- command
+	<- responseChannel
 
 	fmt.Fprintf(w, "ok")
 }
 
 // Provide Prometheus metrics 
 func (server *Server) returnMetric(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "ok")
+	responseChannel := make(chan int)
+
+	// send command for unique IPs count retrieving, recieve value 
+	// in response channel
+	command := Command{GetCount, "", responseChannel}
+	server.commandsChannel <- command
+	count := <- responseChannel
+	fmt.Fprintf(w, strconv.Itoa(count))
 }
 
 func main() {
-	ipsChannel := isIPUnique()
+	commandsChannel := startIPsCounter()
 	server := Server{
-		ipsChan: ipsChannel,
+		commandsChannel: commandsChannel,
 	}
 
 	acceptJsonServerMux := http.NewServeMux()
--- main_test.go
+++ main_test.go
@@ -15,3 +15,44 @@ func TestIsArrayContainValue(t *testing.T){
 		t.Errorf("isArrayContainValue failed, expected %v, got %v", false, exists)
 	}
 }
+
+func TestStartIPsCounter(t *testing.T){
+	commandsChannel := startIPsCounter()
+	responseChannel := make(chan int)
+
+	// check amount of unique IPs before anything will be added
+	command := Command{GetCount, "", responseChannel}
+	commandsChannel <- command
+	count := <- responseChannel
+
+	if count != 0 {
+		t.Errorf("startIPsCounter failed, expected %v, got %v", 0, count)
+	}
+
+	// add new unique IP
+	command = Command{AddIp, "1.2.3.4", responseChannel}
+	commandsChannel <- command
+	<- responseChannel
+
+	// check if unique IPs count is increased
+	command = Command{GetCount, "", responseChannel}
+	commandsChannel <- command
+	count = <- responseChannel
+	if count != 1 {
+		t.Errorf("startIPsCounter failed, expected %v, got %v", 1, count)
+	}
+
+	// add same IP again
+	command = Command{AddIp, "1.2.3.4", responseChannel}
+	commandsChannel <- command
+	<- responseChannel
+
+	// unique IPs count should not be increased
+	command = Command{GetCount, "", responseChannel}
+	commandsChannel <- command
+	count = <- responseChannel
+
+	if count != 1 {
+		t.Errorf("startIPsCounter failed, expected %v, got %v", 1, count)
+	}
+}
